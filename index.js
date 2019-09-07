786d542b86ac5c28b7a87564707f362f22902723
PMHStudio <pmhstudio.pmh@gmail.com>
2019-09-07 10:40:41 +0900

    Electron Frame
    Co-authored-by: ttakkku <ttakkku@outlook.com>
    Co-authored-by: PMH Studio / NoeulNight <ddottybj@naver.com>
    Co-authored-by: 제로ㅣBrazil <hajinlimkr734@gmail.com>

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const electron = require('electron')
+const app = electron.app
+let mainWindow
+
+app.on('ready', () => {
+  if (!mainWindow) createMainWindow()
+})
+
+app.on('activate', () => {
+  if (!mainWindow) createMainWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+function createMainWindow () {
+  mainWindow = new electron.BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    autoHideMenuBar: true
+  }).on('close', () => {
+    mainWindow = null
+  })
+
+  mainWindow.loadFile(path.resolve() + '/src/index.html')
+}
+
+// Let's move to src/index.html!
